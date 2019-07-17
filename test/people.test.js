962b5ff961832467da89a2c20c0717d794040fe4
sepuckett86 <sepuckett86@gmail.com>
2019-07-17 09:55:24 -0700

    add patch route test and finish by id

--- test/people.test.js
+++ test/people.test.js
@@ -4,6 +4,7 @@ const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
+const Person = require('../lib/models/Person');
 
 describe('people routes', () => {
   beforeAll(() => {
@@ -55,6 +56,64 @@ describe('people routes', () => {
           name: 'Susan',
           city: 'Utrecht',
           state: 'Utrecht',
+          profileImage: 'myImage',
+          email: 'myemail@susan.com',
+          __v: 0
+        });
+      });
+  });
+
+  it('gets people', async() => {
+    const person = await Person.create({
+      name: 'Susan',
+      city: 'Utrecht',
+      state: 'Utrecht',
+      profileImage: 'myImage',
+      email: 'myemail@susan.com'
+    });
+
+    return request(app)
+      .get('/api/v1/people')
+      .then(res => {
+        const personJSON = JSON.parse(JSON.stringify(person));
+        expect(res.body).toEqual([personJSON]);
+      });
+  });
+
+  it('gets person by id', async() => {
+    const person = await Person.create({
+      name: 'Susan',
+      city: 'Utrecht',
+      state: 'Utrecht',
+      profileImage: 'myImage',
+      email: 'myemail@susan.com'
+    });
+    
+    return request(app)
+      .get(`/api/v1/people/${person._id}`)
+      .then(res => {
+        const personJSON = JSON.parse(JSON.stringify(person));
+        expect(res.body).toEqual(personJSON);
+      });
+  });
+  it('patches person by id', async() => {
+    const person = await Person.create({
+      name: 'Susan',
+      city: 'Utrecht',
+      state: 'Utrecht',
+      profileImage: 'myImage',
+      email: 'myemail@susan.com'
+    });
+    
+    return request(app)
+      .patch(`/api/v1/people/${person._id}`)
+      .send({ city: 'Portland', state: 'Oregon' })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          name: 'Susan',
+          city: 'Portland',
+          state: 'Oregon',
           profileImage: 'myImage',
           email: 'myemail@susan.com',
           __v: 0
